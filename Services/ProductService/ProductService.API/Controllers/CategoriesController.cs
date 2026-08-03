92e186d3f952ff2bd6160f6262709a79e816b6ef
Marwan <marwanmohamed5174@gmail.com>
2026-08-03 12:25:34 +0300

    products service presentation layer prep [4] #3

---
+++ Services/ProductService/ProductService.API/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.API.Controllers
+{
+    [Route("api/V1/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
---
+++ Services/ProductService/ProductService.API/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.API.Controllers
+{
+    [Route("api/V1/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
