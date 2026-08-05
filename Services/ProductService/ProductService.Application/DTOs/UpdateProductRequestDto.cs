c6092e3c66a3d1b41c32d56716a26831d3255909
Marwan <marwanmohamed5174@gmail.com>
2026-08-05 10:37:23 +0300

    [9] #3

--- Services/ProductService/ProductService.Application/DTOs/UpdateProductRequestDto.cs
+++ Services/ProductService/ProductService.Application/DTOs/UpdateProductRequestDto.cs
@@ -1,9 +1,5 @@
 ﻿using ProductService.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ProductService.Application.DTOs
 {
