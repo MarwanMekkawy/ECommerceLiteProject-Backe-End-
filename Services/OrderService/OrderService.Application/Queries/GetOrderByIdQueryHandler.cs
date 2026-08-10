dbcd12e68e7821505f83a145a80bea712d232da3
Marwan <marwanmohamed5174@gmail.com>
2026-08-10 12:14:37 +0300

    TDD AppLayer GetOrderByIdQueryHandler Passing tests [18] #4

--- Services/OrderService/OrderService.Application/Queries/GetOrderByIdQueryHandler.cs
+++ Services/OrderService/OrderService.Application/Queries/GetOrderByIdQueryHandler.cs
@@ -1,13 +1,14 @@
 ﻿using OrderService.Application.Abstractions;
+using OrderService.Domain.Contracts;
 using OrderService.Domain.Orders;
 
 namespace OrderService.Application.Queries
 {
-    public class GetOrderByIdQueryHandler : IQueryHandler<GetOrderByIdQuery, Order?>
+    public class GetOrderByIdQueryHandler(IOrderRepository orderRepository) : IQueryHandler<GetOrderByIdQuery, Order?>
     {
-        public Task<Order?> HandleAsync(GetOrderByIdQuery query, CancellationToken cancellationToken = default)
+        public async Task<Order?> HandleAsync(GetOrderByIdQuery query, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return await orderRepository.GetByIdUntrackedAsync(query.OrderId, cancellationToken);
         }
     }
 }
