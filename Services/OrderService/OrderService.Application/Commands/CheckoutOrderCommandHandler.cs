775315bd00733f230c4b998a09ff0ab9c756a987
Marwan <marwanmohamed5174@gmail.com>
2026-08-11 13:33:55 +0300

    TDD changing Order bussiness logic & CheckoutCommandHandler passing Tests [30] #4

--- Services/OrderService/OrderService.Application/Commands/CheckoutOrderCommandHandler.cs
+++ Services/OrderService/OrderService.Application/Commands/CheckoutOrderCommandHandler.cs
@@ -8,12 +8,66 @@ using OrderService.Domain.Orders;
 
 namespace OrderService.Application.Commands
 {
-    public class CheckoutOrderCommandHandler(IOrderRepository orderRepository, IProductServiceClient productServiceClient)
+    public class CheckoutOrderCommandHandler(IOrderRepository orderRepository, IProductServiceClient productServiceClient) 
         : ICommandHandler<CheckoutOrderCommand, CheckoutOrderDto>
     {
-        public Task<CheckoutOrderDto> HandleAsync(CheckoutOrderCommand command, CancellationToken cancellationToken)
+        public async Task<CheckoutOrderDto> HandleAsync(CheckoutOrderCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var order = await orderRepository.GetByIdUntrackedAsync(command.OrderId, cancellationToken);
+
+            if (order is null)
+                throw new NotFoundException($"Order with Id {command.OrderId} Was NOT FOUND.");
+
+            var checkoutItems = new List<CheckoutOrderItemDto>();
+            var decreasedItems = new List<OrderItem>();
+
+            try
+            {
+                CurrencyCode? currency = null;
+
+                foreach (var item in order.Items)
+                {
+                    var product = await productServiceClient.GetProductForCheckoutAsync(item.ProductId, cancellationToken);
+
+                    if (currency is null)
+                        currency = product.Currency;
+                    else if (currency != product.Currency)
+                        throw new InvalidOrderException("All order items must use the same currency.");
+
+                    await productServiceClient.DecreaseStockAsync(item.ProductId, item.Quantity, cancellationToken);
+
+                    decreasedItems.Add(item);
+
+                    var lineTotal = product.Price * item.Quantity;
+
+                    checkoutItems.Add(new CheckoutOrderItemDto
+                    {
+                        ProductId = item.ProductId,
+                        Quantity = item.Quantity,
+                        UnitPrice = product.Price,
+                        Total = lineTotal
+                    });
+                }
+
+                var total = checkoutItems.Sum(x => x.Total);
+
+                return new CheckoutOrderDto
+                {
+                    OrderId = order.Id,
+                    Items = checkoutItems,
+                    Total = total,
+                    Currency = currency!.Value
+                };
+            }
+            catch
+            {
+                foreach (var item in decreasedItems)
+                {
+                    await productServiceClient.IncreaseStockAsync(item.ProductId, item.Quantity,  cancellationToken);
+                }
+
+                throw;
+            }
         }
     }
 }
